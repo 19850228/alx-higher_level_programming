5a7c3537b999846816d7167b310e8e0484bf75ac
19850228 <fmthombothi@yahoo.com>
2023-12-02 00:57:26 -0800

    Python import modules

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-element_at = __import__('1-element_at').element_at
-
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
+def element_at(my_list, idx):
+    return(my_list[idx] if 0 <= idx < len(my_list) else "None")
--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-divisible_by_2 = __import__('10-divisible_by_2').divisible_by_2
+def divisible_by_2(my_list=[]):
+    # Use list comprehension to create a new list with True or False
+    # based on whether each element is a multiple of 2
+    return [True if x % 2 == 0 else False for x in my_list]
 
-my_list = [0, 1, 2, 3, 4, 5, 6]
-list_result = divisible_by_2(my_list)
-
-i = 0
-while i < len(list_result):
-    print("{:d} {:s} divisible by 2".format(my_list[i], "is" if list_result[i] else "is not"))
-    i += 1
+# Uncomment the following lines for testing
+# my_list = [0, 1, 2, 3, 4, 5, 6]
+# list_result = divisible_by_2(my_list)
+# i = 0
+# while i < len(list_result):
+#     print("{:d} {:s} divisible by 2".format(my_list[i], "is" if list_result[i] else "is not"))
+#     i += 1
