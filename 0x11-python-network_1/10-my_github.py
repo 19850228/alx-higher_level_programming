2301603a69d396bf59ef9b1345dba85bc4787937
19850228 <fmthombothi@yahoo.com>
2024-03-28 11:01:50 -0700

    network

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Script that uses Basic Authentication with a personal access token
+to access the GitHub API and display the user ID.
+"""
+
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+if __name__ == "__main__":
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
